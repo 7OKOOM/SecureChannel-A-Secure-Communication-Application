104e1c1beb86caa7e38174c0ac4e456d4fcdb4df
7OKOOM <hakamsaed06@gmail.com>
2026-06-20 16:58:07 +0300

    ChaCha20 and Poly1305 AEAD implementation

--- chacha20_poly1305.py
+++ chacha20_poly1305.py
@@ -1,92 +1,116 @@
 import struct
 
-def rotl32(v, c):
-    return ((v << c) & 0xFFFFFFFF) | (v >> (32 - c))
+class ChaCha20:
 
-def quarterRound(x, a, b, c, d):
-    x[a] = (x[a] + x[b]) & 0xFFFFFFFF
-    x[d] ^= x[a]
-    x[d] = rotl32(x[d], 16)
+    def __init__(self, key, nonce):
+        assert len(key) == 32
+        assert len(nonce) == 12
+        self.key = key
+        self.nonce = nonce
 
-    x[c] = (x[c] + x[d]) & 0xFFFFFFFF
-    x[b] ^= x[c]
-    x[b] = rotl32(x[b], 12)
+    @staticmethod
+    def _rotl32(v, c):
+        return ((v << c) & 0xFFFFFFFF) | (v >> (32 - c))
 
-    x[a] = (x[a] + x[b]) & 0xFFFFFFFF
-    x[d] ^= x[a]
-    x[d] = rotl32(x[d], 8)
+    @classmethod
+    def _quarter_round(cls, x, a, b, c, d):
+        x[a] = (x[a] + x[b]) & 0xFFFFFFFF
+        x[d] ^= x[a]
+        x[d] = cls._rotl32(x[d], 16)
 
-    x[c] = (x[c] + x[d]) & 0xFFFFFFFF
-    x[b] ^= x[c]
-    x[b] = rotl32(x[b], 7)
+        x[c] = (x[c] + x[d]) & 0xFFFFFFFF
+        x[b] ^= x[c]
+        x[b] = cls._rotl32(x[b], 12)
 
-def chacha20_block(key, counter, nonce):
-    constants = struct.unpack('<4I', b'expand 32-byte k')
-    key_words = struct.unpack('<8I', key)
-    counter_bytes = struct.pack('<I', counter)      
-    counter_nonce = struct.unpack('<4I', counter_bytes + nonce)  
-    state = list(constants) + list(key_words) + list(counter_nonce)
-    working_state = state.copy()
+        x[a] = (x[a] + x[b]) & 0xFFFFFFFF
+        x[d] ^= x[a]
+        x[d] = cls._rotl32(x[d], 8)
 
-    for j in range(10):
-        quarterRound(working_state, 0, 4, 8, 12)
-        quarterRound(working_state, 1, 5, 9, 13)
-        quarterRound(working_state, 2, 6, 10, 14)
-        quarterRound(working_state, 3, 7, 11, 15)
-        quarterRound(working_state, 0, 5, 10, 15)
-        quarterRound(working_state, 1, 6, 11, 12)
-        quarterRound(working_state, 2, 7, 8, 13)
-        quarterRound(working_state, 3, 4, 9, 14)
+        x[c] = (x[c] + x[d]) & 0xFFFFFFFF
+        x[b] ^= x[c]
+        x[b] = cls._rotl32(x[b], 7)
 
-    for i in range(16):
-        working_state[i] = (working_state[i] + state[i]) & 0xFFFFFFFF
+    def block(self, counter):
+        """Generate one 64-byte keystream block for the given counter value."""
+        constants = struct.unpack('<4I', b'expand 32-byte k')
+        key_words = struct.unpack('<8I', self.key)
+        counter_bytes = struct.pack('<I', counter)
+        counter_nonce = struct.unpack('<4I', counter_bytes + self.nonce)
+        state = list(constants) + list(key_words) + list(counter_nonce)
+        working_state = state.copy()
 
-    return struct.pack('<16I', *working_state)
+        for _ in range(10):
+            self._quarter_round(working_state, 0, 4, 8, 12)
+            self._quarter_round(working_state, 1, 5, 9, 13)
+            self._quarter_round(working_state, 2, 6, 10, 14)
+            self._quarter_round(working_state, 3, 7, 11, 15)
+            self._quarter_round(working_state, 0, 5, 10, 15)
+            self._quarter_round(working_state, 1, 6, 11, 12)
+            self._quarter_round(working_state, 2, 7, 8, 13)
+            self._quarter_round(working_state, 3, 4, 9, 14)
+
+        for i in range(16):
+            working_state[i] = (working_state[i] + state[i]) & 0xFFFFFFFF
+
+        return struct.pack('<16I', *working_state)
+
+    def encrypt(self, data, counter=1):
+        """XOR data with the keystream starting at `counter`. Symmetric: also decrypts."""
+        output = bytearray()
+        for i in range(0, len(data), 64):
+            keystream = self.block(counter)
+            block = data[i:i + 64]
+            for j in range(len(block)):
+                output.append(block[j] ^ keystream[j])
+            counter += 1
+        return bytes(output)
 
 
-def poly1305_mac(key, message):
-    r = int.from_bytes(key[:16], byteorder='little') & 0x0FFFFFFC0FFFFFFC0FFFFFFC0FFFFFFF
-    s = int.from_bytes(key[16:], byteorder='little')
-    p = 2**130 - 5
-    acc = 0
+class Poly1305:
+    """Poly1305 one-time MAC (RFC 8439, section 2.5)."""
 
-    for i in range(0, len(message), 16):
-        block = message[i:i+16]
-        n = int.from_bytes(block + b'\x01', byteorder='little')
-        acc = (acc + n) * r % p
+    def __init__(self, key):
+        assert len(key) == 32
+        self.r = int.from_bytes(key[:16], byteorder='little') & 0x0FFFFFFC0FFFFFFC0FFFFFFC0FFFFFFF
+        self.s = int.from_bytes(key[16:], byteorder='little')
 
-    acc = (acc + s) % (2**128)
-    return acc.to_bytes(16, byteorder='little')
+    @staticmethod
+    def pad16(data):
+        padding_length = (16 - (len(data) % 16)) % 16
+        return b'\x00' * padding_length
+
+    def mac(self, message):
+        p = 2 ** 130 - 5
+        acc = 0
+        for i in range(0, len(message), 16):
+            block = message[i:i + 16]
+            n = int.from_bytes(block + b'\x01', byteorder='little')
+            acc = (acc + n) * self.r % p
+        acc = (acc + self.s) % (2 ** 128)
+        return acc.to_bytes(16, byteorder='little')
 
 
-def pad16(data):
-    padding_length = (16 - (len(data) % 16)) % 16
-    return b'\x00' * padding_length
+class ChaCha20Poly1305:
+   
+    def __init__(self, key):
+        assert len(key) == 32
+        self.key = key
 
+    def encrypt(self, nonce, plaintext, associated_data=b''):
+        assert len(nonce) == 12
 
-def chacha20_aead_encrypt(key, nonce, plaintext, associated_data=b''):
-    assert len(key) == 32
-    assert len(nonce) == 12
+        cipher = ChaCha20(self.key, nonce)
+        poly_key = cipher.block(0)[:32]
+        ciphertext = cipher.encrypt(plaintext, counter=1)
 
-    poly_key = chacha20_block(key, 0, nonce)[:32]
-    ciphertext = bytearray()
-    counter = 1
-    for i in range(0, len(plaintext), 64):
-        key_stream = chacha20_block(key, counter, nonce)
-        block = plaintext[i:i+64]
-        for j in range(len(block)):
-            ciphertext.append(block[j] ^ key_stream[j])
-        counter += 1
-    ciphertext = bytes(ciphertext)
+        mac_data = bytearray()
+        mac_data += associated_data
+        mac_data += Poly1305.pad16(associated_data)
+        mac_data += ciphertext
+        mac_data += Poly1305.pad16(ciphertext)
+        mac_data += struct.pack('<Q', len(associated_data))
+        mac_data += struct.pack('<Q', len(ciphertext))
 
-    mac_data = bytearray()
-    mac_data += associated_data
-    mac_data += pad16(associated_data)
-    mac_data += ciphertext
-    mac_data += pad16(ciphertext)
-    mac_data += struct.pack('<Q', len(associated_data))
-    mac_data += struct.pack('<Q', len(ciphertext))
+        tag = Poly1305(poly_key).mac(mac_data)
 
-    tag = poly1305_mac(poly_key, mac_data)
-
-    return ciphertext, tag
+        return ciphertext, tag
