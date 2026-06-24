f6a6bd2e6a7145ca4369600e7656c3f5a27c8126
Wadee Owais <=>
2026-06-24 23:08:47 +0300

    phase 1 is approximately ready

--- client.py
+++ client.py
@@ -1,18 +1,42 @@
 import socket
 
-import config
-from X25519 import X25519
+from hdkf import HKDF
+from hmac import HMAC
 
+from X25519 import X25519
+import os
+from secret import Secret
+PROTOCOL_VERSION = b"SecureChannel"
+CLIENT_ID        = b"client"
+SERVER_ID        = b"server"
 
 def run_client(host='127.0.0.1', port=65432):
     # Create a TCP socket
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
+        # PHASE A: Handshake (Implement X25519 + HMAC Authentication)
         exchanger = X25519()
-        B = exchanger.generate_public_key()
+        private_key = bytearray(os.urandom(32))
+        B = exchanger.generate_public_key(private_key)
+        s.sendall(B)
+        A =    s.recv(32)
+        shared_secret = exchanger.compute_shared_secret(private_key,A)
+        mac_gen = HMAC()
+        mac = mac_gen.hmac(Secret.PSK,PROTOCOL_VERSION+CLIENT_ID+SERVER_ID+A+B)
+        s.sendall(mac)
+        mac_rcv = s.recv(32)
+        if mac_rcv != mac:
+            print("Server hasn't been authenticated. Closing connection.")
+            s.close()
+            return
+        print("mac received correctly!")
+        mixer = HKDF()
+        key = mixer.hkdf(Secret.PSK,shared_secret,"p1",88)
+        key_server_to_client = key[:32]
+        key_client_to_server = key[32:64]
+        nonce_server_to_client = key[64:76]
+        nonce_client_to_server = key[76:]
 
-        # PHASE A: Handshake
-        # TODO: Perform key exchange and verify PSK-based transcript
 
         # PHASE B: Secure Messaging
         message = b"Hello, Secure World!"
--- server.py
+++ server.py
@@ -1,4 +1,15 @@
+import os
 import socket
+
+from hdkf import HKDF
+from hmac import HMAC
+
+from X25519 import X25519
+from secret import Secret
+
+PROTOCOL_VERSION = b"SecureChannel"
+CLIENT_ID        = b"client"
+SERVER_ID        = b"server"
 
 def run_server(host='127.0.0.1', port=65432):
     # Create a TCP socket
@@ -7,12 +18,32 @@ def run_server(host='127.0.0.1', port=65432):
         s.listen()
         print(f"Server listening on {host}:{port}")
 
+        exchanger = X25519()
+        private_key = bytearray(os.urandom(32))
         conn, addr = s.accept()
         with conn:
-            print(f"Connected by {addr}")
-
             # PHASE A: Handshake (Implement X25519 + HMAC Authentication)
-            # TODO: Perform key exchange and verify PSK-based transcript
+            print(f"Connected by {addr}")
+            B = conn.recv(32)
+            shared_secret = exchanger.compute_shared_secret(private_key, B)
+            A = exchanger.generate_public_key(private_key)
+            conn.sendall(A)
+            mac_gen = HMAC()
+            mac = mac_gen.hmac(Secret.PSK,PROTOCOL_VERSION+CLIENT_ID+SERVER_ID+A+B)
+            mac_rcv = conn.recv(32)
+            conn.sendall(mac)
+            if mac_rcv != mac:
+                print("Client hasn't been authenticated. Closing connection.")
+                conn.close()
+                return
+            print("mac received correctly!")
+            mixer = HKDF()
+            key = mixer.hkdf(Secret.PSK,shared_secret,"p1",88)
+            key_server_to_client = key[:32]
+            key_client_to_server = key[32:64]
+            nonce_server_to_client = key[64:76]
+            nonce_client_to_server = key[76:]
+
 
             # PHASE B: Secure Messaging (ChaCha20-Poly1305)
             while True:
